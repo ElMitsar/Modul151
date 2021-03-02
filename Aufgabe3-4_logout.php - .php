d81765cbf98d4bf7824cf04bfc3202590f3d5058
ElMitsar <Simon_Odermatt2@sluz.ch>
2021-03-02 20:54:53 +0100

    Add files via upload

---
+++ Aufgabe3-4_logout.php - .php
@@ -0,0 +1,19 @@
+<?php
+session_destory();
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+  </head>
+  <script>
+  alert("Erfolgreich ausgelogt");
+  window.location.href = 'login.php'
+  </script>
+  <body>
+
+  </body>
+</html>
